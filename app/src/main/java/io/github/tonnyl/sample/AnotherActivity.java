94d20758e02430c31104ab913de6cba8af8c85c1
Samuel Agbede <sammyplexus@gmail.com>
2019-07-11 15:08:21 +0100

    feat : Add listener for when user is done viewing WhatsNew dialog

--- app/src/main/java/io/github/tonnyl/sample/AnotherActivity.java
+++ app/src/main/java/io/github/tonnyl/sample/AnotherActivity.java
@@ -2,34 +2,55 @@ package io.github.tonnyl.sample;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.util.Log;
 import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import io.github.tonnyl.whatsnew.WhatsNew;
+import io.github.tonnyl.whatsnew.WhatsNewKt;
+import io.github.tonnyl.whatsnew.interfaces.ViewFinishedListener;
 import io.github.tonnyl.whatsnew.item.WhatsNewItem;
+import io.github.tonnyl.whatsnew.util.PresentationOption;
+
 
 /**
  * Created by lizhaotailang on 01/12/2017.
  */
 
-public class AnotherActivity extends AppCompatActivity {
+public class AnotherActivity extends AppCompatActivity{
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         findViewById(R.id.textView).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                WhatsNew.newInstance(
+                WhatsNew instance = WhatsNew.newInstance(
                         new WhatsNewItem("Nice Icons", "Completely customize colors, texts and icons.", R.drawable.ic_heart),
                         new WhatsNewItem("Such Easy", "Setting this up only takes 2 lines of code, impressive you say?", R.drawable.ic_thumb_up),
                         new WhatsNewItem("Very Sleep", "It helps you get more sleep by writing less code.", R.drawable.ic_satisfied_face),
                         new WhatsNewItem("Text Only", "No icons? Just go with plain text.", WhatsNewItem.NO_IMAGE_RES_ID)
-                ).presentAutomatically(AnotherActivity.this);
+                );
+
+                instance.setPresentationOption(PresentationOption.DEBUG);
+
+                WhatsNewKt.whenViewed(instance, new ViewFinishedListener(){
+                    @Override
+                    public void onViewFinished() {
+                        // Any code (e.g starting a new activity) written here would be executed when the user is done viewing the whatsNew dialog
+                    }
+                })
+                .presentAutomatically(AnotherActivity.this);
+
+
+
+
+
 
 //                WhatsNew whatsNew = WhatsNew.newInstance(
 //                        new WhatsNewItem("Nice Icons", "Completely customize colors, texts and icons.", R.drawable.ic_heart),
---
+++ whatsnew/src/main/java/io/github/tonnyl/whatsnew/interfaces/ViewFinishedListener.java
@@ -0,0 +1,5 @@
+package io.github.tonnyl.whatsnew.interfaces;
+
+public interface ViewFinishedListener {
+    void onViewFinished();
+}
