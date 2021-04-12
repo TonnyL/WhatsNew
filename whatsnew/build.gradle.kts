8d7dd50c27a893362c8558819ccc7d785c47757a
tonnyl <lizhaotailang@gmail.com>
2021-04-13 01:04:11 +0800

    Fix publication

--- whatsnew/build.gradle.kts
+++ whatsnew/build.gradle.kts
@@ -6,8 +6,8 @@ plugins {
     id("com.android.library")
     kotlin("android")
     id("kotlin-parcelize")
-    `maven-publish`
-    id("signing")
+    id("org.jetbrains.dokka")
+    id("com.vanniktech.maven.publish")
 }
 
 android {
@@ -55,94 +55,24 @@ dependencies {
     androidTestImplementation(Deps.AndroidTest.espressoCore)
 }
 
-// Disable creating javadocs
-task<Javadoc>("withType") {
-    isEnabled = false
-}
-
-val androidSourcesJar by tasks.register<Jar>("androidSourcesJar") {
-    archiveClassifier.set("sources")
-    from(android.sourceSets.getByName("main").java.srcDirs)
-}
-
-var signingKeyId = ""
-var signingPassword = ""
-var signingSecretKeyRingFile = ""
 var ossrhUsername = ""
 var ossrhPassword = ""
 
 val secretPropsFile = project.rootProject.file("local.properties")
 if (secretPropsFile.exists()) {
     val properties = loadProperties("${rootDir}/local.properties")
-    signingKeyId = properties.propertyString("signing.keyId") ?: ""
-    signingPassword = properties.propertyString("signing.password") ?: ""
-    signingSecretKeyRingFile = properties.propertyString("signing.secretKeyRingFile") ?: ""
     ossrhUsername = properties.propertyString("ossrhUsername") ?: ""
-    ossrhPassword = properties.getProperty("ossrhPassword")
+    ossrhPassword = properties.propertyString("ossrhPassword") ?: ""
 } else {
     println("No props file, loading env vars")
-    signingKeyId = System.getenv("SIGNING_KEY_ID") ?: ""
-    signingPassword = System.getenv("SIGNING_PASSWORD") ?: ""
-    signingSecretKeyRingFile = System.getenv("SIGNING_SECRET_KEY_RING_FILE") ?: ""
     ossrhUsername = System.getenv("OSSRH_USERNAME") ?: ""
     ossrhPassword = System.getenv("OSSRH_PASSWORD") ?: ""
 }
 
-ext["signing.keyId"] = signingKeyId
-ext["signing.password"] = signingPassword
-ext["signing.secretKeyRingFile"] = signingSecretKeyRingFile
-ext["ossrhUsername"] = ossrhUsername
-ext["ossrhPassword"] = ossrhPassword
-
 publishing {
-    publications {
-        create<MavenPublication>("release") {
-            groupId = "io.github.tonnyl"
-            artifactId = "whatsnew"
-            version = "0.1.3"
-
-            artifact("$buildDir/outputs/aar/${project.getName()}-release.aar")
-            artifact(androidSourcesJar)
-
-            pom {
-                name.set("WhatsNew")
-                description.set("WhatsNew automatically displays a short description of the new features when users update your app.")
-                url.set("https://github.com/TonnyL/WhatsNew")
-                licenses {
-                    license {
-                        name.set("MIT License")
-                        url.set("https://opensource.org/licenses/MIT")
-                    }
-                }
-                developers {
-                    developer {
-                        id.set("TonnyL")
-                        name.set("Li Zhao Tai Lang")
-                        email.set("lizhaotailang@gmail.com")
-                    }
-                }
-                scm {
-                    connection.set("scm:git:github.com/TonnyL/WhatsNew.git")
-                    developerConnection.set("scm:git:ssh://github.com/TonnyL/WhatsNew.git")
-                    url.set("https://github.com/TonnyL/WhatsNew")
-                }
-                withXml {
-                    val dependenciesNode = asNode().appendNode("dependencies")
-
-                    project.configurations.implementation.allDependencies.forEach {
-                        val dependencyNode = dependenciesNode.appendNode("dependency")
-                        dependencyNode.appendNode("groupId", it.group)
-                        dependencyNode.appendNode("artifactId", it.name)
-                        dependencyNode.appendNode("version", it.version)
-                    }
-                }
-            }
-        }
-    }
     repositories {
-        maven {
-            name = "mavencentral" // Sonatype / MavenCentral
-            val releasesRepoUrl = "https://s01.oss.sonatype.org/content/repositories/releases/"
+        withType<MavenArtifactRepository>() {
+            val releasesRepoUrl = "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
             val snapshotsRepoUrl = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
             url = URI(
                     if (version.toString().endsWith("SNAPSHOT")) {
@@ -151,6 +81,7 @@ publishing {
                         releasesRepoUrl
                     }
             )
+
             credentials {
                 username = ossrhUsername
                 password = ossrhPassword
@@ -159,6 +90,12 @@ publishing {
     }
 }
 
-signing {
-    sign(publishing.publications)
+mavenPublish {
+    releaseSigningEnabled = true
+    nexus {
+        baseUrl = "https://s01.oss.sonatype.org" // defaults to "https://oss.sonatype.org/service/local/"
+        stagingProfile = "io.github.tonnyl" // defaults to the SONATYPE_STAGING_PROFILE Gradle property or the GROUP Gradle Property if not set
+        repositoryUsername = ossrhUsername // defaults to the mavenCentralRepositoryUsername Gradle Property
+        repositoryPassword = ossrhPassword // defaults to the mavenCentralRepositoryPassword Gradle Property
+    }
 }
